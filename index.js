6f79a82dd003531f7fcff2f32b52dc3be0f8e76b
dharmendra_17 <dharmendra17893@gmail.com>
2017-07-24 19:15:10 +0530

    added the files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var _ = require('lodash');
-//var pdfreader = require('pdfreader');
 var promiss = require('bluebird');
 var fs = require('fs');
-var path = require('path')
+var path = require('path');
+var PDFParser = require("pdf2json/pdfparser");
+var async = require('async');
 
 function XcdPdf(options){
  this.options = options || {};
@@ -10,20 +11,46 @@ function XcdPdf(options){
 }
 
 function getTextCordinates(option, error, next){
-	next(error);
+	if(error){
+		return next(error, null);	
+	}
+	else {
+		var itemPromiss = [];
+		var pdfParser = new PDFParser();
+		  pdfParser.on("pdfParser_dataError", function(error){
+		  	next(error, null)
+		  });
+		  pdfParser.on("pdfParser_dataReady", function (pdfData){
+		   var pageNumber = 0;
+		   _.map(_.get(pdfData, 'formImage.Pages'), function(page){
+		   	var obj = {
+		   		page : ++pageNumber,
+		   		texts : []
+		   	}
+		   	_.forEach(_.get(page, 'Texts'), function(text){
+		   		var item = text;
+      			item.text = decodeURIComponent(_.get(_.first(_.get(item, 'R')), 'T'));
+      			if(_.get(option,'text').indexOf(item.text)>0)
+      			{
+      				obj.texts.push(item);
+      			}
+		   	})
+		   	itemPromiss.push(obj)
+		   })
+		   next(null, itemPromiss)
+		  });
+		  pdfParser.loadPDF(_.get(option, 'pdfFile'), 1);
+
+	}
 }
 
 
 function logValue(option, handler, callback){
 
 	if (!fs.existsSync(_.get(option, 'pdfFile'))) {
-	    // Do something
-	     console.log('Checking the file');
-	     return handler(option, 'unable to find the file', callback)
+	    return handler(option, 'unable to find the file', callback)
 	}
-
 	return handler(option, null, callback)
-
 }
 
 module.exports = XcdPdf;
--- test.js
+++ test.js
@@ -2,8 +2,9 @@ var XcdPdf = require('./index.js');
 var xcdPdf = new XcdPdf();
 
 var option = {
-	pdfFile : 'fdf'
+	pdfFile : './response.pdf',
+	text : ['{{SS:#1:signature}}', '1/1/1800']
 }
 xcdPdf.getTextCordinates(option, function(err, data){
-	console.log(err, data)
+	console.log(err, JSON.stringify(data))
 });
