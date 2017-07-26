e9b3c0ebccc8fb9dcb928ea77a73ab4bde0c0b41
Amit Vishwakarma <mail.amitvishwakarma@gmail.com>
2017-07-26 15:06:26 +0530

    replace pdf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var async = require('async');
 function XcdPdf(options){
  this.options = options || {};
  this.getTextCordinates = _.partial(logValue, _, getTextCordinates, _);
- this.validateText = _.partial(logValue, _, validateText, _);
+ this.changeTextColor = _.partial(logValue, _, changeTextColor, _);
 }
 
 function getTextCordinates(option, error, next){
@@ -29,6 +29,8 @@ function getTextCordinates(option, error, next){
 		   		page : ++pageNumber,
 		   		texts : []
 		   	}
+
+
 		   	_.forEach(_.get(page, 'Texts'), function(text){
 		   		var item = text;
       			item.text = decodeURIComponent(_.get(_.first(_.get(item, 'R')), 'T'));
@@ -46,35 +48,21 @@ function getTextCordinates(option, error, next){
 }
 
 
-function validateText(option, error, next){
+function changeTextColor(option, error, next){
 	if(error){
 		return next(error, null);	
 	}
 	else {
-		var items = [];
+		console.log(option);
 		var pdfParser = new PDFParser();
 		  pdfParser.on("pdfParser_dataError", function(error){
 		  	next(error, null)
 		  });
 		  pdfParser.on("pdfParser_dataReady", function (pdfData){
-		   var pageNumber = 0;
-
-		   _.map(_.get(pdfData, 'formImage.Pages'), function(page){
-		   	var obj = {
-		   		page : ++pageNumber,
-		   		text : {}
-		   	}
-		   	_.forEach(_.get(page, 'Texts'), function(text){
-		   		var item = text;
-      			item.text = decodeURIComponent(_.get(_.first(_.get(item, 'R')), 'T'));
-      			if(_.get(option,'text').indexOf(item.text)>-1)
-      			{
-      				obj.texts.push(item);
-      			}
-		   	})
-		   	items.push(obj)
-		   })
-		   next(null, items)
+		   //var pageNumber = 0;
+		   fs.writeFile("./F1040EZ.fields.json", JSON.stringify(pdfData));
+		  
+		   //next(null, items)
 		  });
 		  pdfParser.loadPDF(_.get(option, 'pdfFile'), 1);
 	}
--- test.js
+++ test.js
@@ -2,9 +2,14 @@ var XcdPdf = require('./index.js');
 var xcdPdf = new XcdPdf();
 
 var option = {
-	pdfFile : './response.pdf',
-	text : ['{{SS:#1:signature}}', '1/1/1800']
+	pdfFile : './test.pdf',
+	text : ['{{SS:#1:signature}}', '1/1/1800'],
+	colorRGB : '0 0 0'
 }
-xcdPdf.getTextCordinates(option, function(err, data){
+// xcdPdf.getTextCordinates(option, function(err, data){
+// 	console.log(err, JSON.stringify(data))
+// });
+
+xcdPdf.changeTextColor(option, function(err, data){
 	console.log(err, JSON.stringify(data))
 });
